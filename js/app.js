fc3794fb92e58acc56df35191ec69f96a7f94c26
ayman <ayman.mosstafa@gmail.com>
2018-07-05 00:23:42 +0200

    refact: making enemy class function

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+    constructor(x, y, speed){
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/enemy-bug.png';
+    }
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+update (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+render () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
