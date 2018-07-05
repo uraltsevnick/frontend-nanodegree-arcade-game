5362341f90870748e2a8c764a8b05cb28d1c0146
ayman <ayman.mosstafa@gmail.com>
2018-07-05 10:07:42 +0200

    refact: iniating enemy speed

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ class Enemy {
         this.x = x;
         this.y = y;
         this.sprite = 'images/enemy-bug.png';
+        this.speed = Math.floor((Math.random() * 200) + 100);
     }
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -17,6 +18,7 @@ update (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -61,25 +63,15 @@ class Player {
 // Now instantiate your objects.
 
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [];
-let Enemy1 = new Enemy(-1,65,20);
-allEnemies.push(Enemy1);
-let Enemy2 = new Enemy(-1,150,20);
-allEnemies.push(Enemy2);
-let Enemy3 = new Enemy(-1,230,20);
-allEnemies.push(Enemy3);
+const allEnemies = [new Enemy(-1,65,20), new Enemy(-1,150,20), new Enemy(-1,230,20)];
+
 // Place the player object in a variable called player
 let player = new Player(200,405);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+    let allowedKeys = { 37: 'left', 38: 'up', 39: 'right', 40: 'down' };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
